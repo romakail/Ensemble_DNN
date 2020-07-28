d8dc1ccd7de98b478e17180d4628b7d06d9421fa
romakail <roman.kail@mail.ru>
2020-07-28 12:08:41 +0000

    Upgraded notebook which checs sanity of gradient

--- models/convfc.py
+++ models/convfc.py
@@ -2,7 +2,7 @@ import math
 import torch.nn as nn
 
 __all__ = [
-    'ConvFC',
+    'ConvFC', 'ConvFCSimple'
 ]
 
 
@@ -42,6 +42,45 @@ class ConvFCBase(nn.Module):
         x = self.fc_part(x)
         return x
 
+class ConvFCSimpleBase(nn.Module):
+
+    def __init__(self, num_classes):
+        super(ConvFCSimpleBase, self).__init__()
+        self.conv_part = nn.Sequential(
+            nn.Conv2d(3, 8, kernel_size=3, padding=2),
+            nn.ReLU(True),
+            nn.MaxPool2d(kernel_size=3, stride=1),
+            nn.Conv2d(8, 8, kernel_size=3, padding=1),
+            nn.ReLU(True),
+            nn.MaxPool2d(3, 2),
+            nn.Conv2d(8, 8, kernel_size=3, padding=1),
+            nn.ReLU(True),
+            nn.MaxPool2d(3, 2),
+        )
+        self.fc_part = nn.Sequential(
+            nn.Linear(392, 100),
+            nn.ReLU(True),
+            nn.Linear(100, num_classes)
+        )
+
+        # Initialize weights
+        for m in self.conv_part.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+                m.bias.data.zero_()
+
+    def forward(self, x):
+        x = self.conv_part(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc_part(x)
+        return x
+
+
 class ConvFC:
     base = ConvFCBase
+    kwargs = {}
+    
+class ConvFCSimple:
+    base = ConvFCSimpleBase
     kwargs = {}
